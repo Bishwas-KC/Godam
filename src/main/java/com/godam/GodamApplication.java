08db0a0fd355d72e0e7d0e116e0397ae884699f6
Bishwas KC <bishwaskc777@gmail.com>
2024-11-26 14:49:30 +0545

    firstcommit

---
+++ src/main/java/com/godam/GodamApplication.java
@@ -0,0 +1,13 @@
+package com.godam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/godam/GodamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
